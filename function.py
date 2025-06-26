af7ea496586a91da178e685ef44d60355ee0b2e1
Sufyan <muhammadsufyan11011@gmail.com>
2025-06-26 06:33:36 +0500

    using insert the funciton of for loop

--- function.py
+++ function.py
@@ -1,24 +1,38 @@
-def greet(name):
-	print ("assalam o alikim",name)
-greet("khan")
-def add(a,b):
-	return a+b
-print(add(2,4))
-def even_check(number):
-	if number % 2==0:
-		return "SUFYAN IS GOOD"
-	else:
-		return "SUFYAN IS NOT GOOD"
-print(even_check(4))
-print(even_check(1))
+# def greet(name):
+# 	print ("assalam o alikim",name)
+# greet("khan")
+# def add(a,b):
+# 	return a+b
+# print(add(2,4))
+# def even_check(number):
+# 	if number % 2==0:
+# 		return "SUFYAN IS GOOD"
+# 	else:
+# 		return "SUFYAN IS NOT GOOD"
+# print(even_check(4))
+# print(even_check(1))
 
 
-def pehla_even_no(numbers):
-	for num  in numbers:
-		if num   % 2==0:
-			return num
+# def pehla_even_no(numbers):
+# 	for num  in numbers:
+# 		if num   % 2==0:
+# 			return num
 		
-	return None
-print(pehla_even_no([1,3,5,8]))
+# 	return None
+# print(pehla_even_no([1,3,5,8]))
 
-print(pehla_even_no([1,3,5]))
+# print(pehla_even_no([1,3,5]))
+
+
+def squre(num):
+	if num %2==0:
+		print("Even number")
+	else:
+		print("Odd number")
+squre(4)
+squre(3)
+
+
+
+
+
